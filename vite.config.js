5be68aecf00137e36f7a59109f81618ced4bbeec
Danpa_cho <danpa725@cau.ac.kr>
2022-10-05 16:21:38 +0900

    Setup: solve eslint error at `vite.config`

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vite"
 
 export default defineConfig({
     server: {
         port: 3000,
         strictPort: true,
     },
-});
+})
